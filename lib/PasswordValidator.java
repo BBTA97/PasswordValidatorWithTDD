a072d71fcaa9efaeb61fcf9f2feb996f36811614
BBTA97 <patcharada.kh@ku.th>
2025-07-03 22:14:24 +0700

    Add files via upload

--- lib/PasswordValidator.java
+++ lib/PasswordValidator.java
@@ -1,19 +1,46 @@
 package lib;
+import javax.management.ConstructorParameters;
 
 public class PasswordValidator {
-
+    
     /**
      * คุณจะต้องเขียน Javadoc ที่สมบูรณ์ที่นี่ในอนาคต
      * เพื่ออธิบายกฎการทำงานของเมธอด
-     */
+     * @param รับค่า รับค่าเป็นString
+     * @return ส่งค่ากลับไปยัง PasswordStrength เพื่อตรวจสอบเงื่อนไขว่าเข้า INVALID,WEAK,MEDIUMหรือSTRONG
+     * @throws IllegalArgumentException ถ้ารับค่า null หรือ empty
+     */ 
+
     // TODO: แก้ไข return type ของเมธอดนี้ให้ถูกต้อง
-    public static void validate(String password) { // Function Type ให้เป็น PasswordStrength 
+    public static PasswordStrength validate(String password) {
+        Boolean LowerCase=false, digit=false, UpperCase=false, SpecialChar=false; 
         
-        // ส่วนของ Implementation ที่คุณต้องเขียน
-        // ...
-        
-        int minLength = 8 // TODO: มีอะไรขาดหายไปที่บรรทัดนี้?
-        
-        return null ; // TODO: การคืนค่านี้ถูกต้องหรือไม่?
+        for (char c : password.toCharArray()) {
+           if (Character.isLowerCase(c)) {
+                LowerCase = true;
+            } else if (Character.isDigit(c)) {
+                digit = true;
+            } else if (Character.isUpperCase(c)) {
+                UpperCase = true;
+            } else if (c == '-' || c == '_') {
+                SpecialChar = true;
+            }
+        }
+
+        if(password == null || password.length()<8 )
+            return PasswordStrength.INVALID;
+
+        if (LowerCase && UpperCase && digit && SpecialChar)
+        return PasswordStrength.STRONG;
+
+        if (LowerCase && UpperCase && digit)
+            return PasswordStrength.MEDIUM;
+
+        return PasswordStrength.WEAK;
     }
 }
+
+    //ระดับ INVALID เป็นค่าว่างหรือสั้นเกินไป
+    //ระดับ WEAK ตัวอักษรตัวเล็กกับตัวเลข ตัวอัษรพิมพ์ใหญ่กับตัวเลข ตัวอักษรเล็กกับตัวอักษรใหญ่
+    //ระดับ MEDIUM มีตัวเลข ตัวอักษรเล็กและอักษรพิมพ์ใหญ่
+    //ระดับ STRONG มีตัวเลข ตัวอักษรเล็ก ตัวอักษรพิมพ์ใหญ่ และspecialchar
--- lib/PasswordValidatorTest.java
+++ lib/PasswordValidatorTest.java
@@ -10,10 +10,30 @@ public class PasswordValidatorTest{
         if (result1 == PasswordStrength.INVALID) {
             System.out.println("Test Case 1 Passed: Short password is INVALID.");
         } else {
-            System.out.println("Test Case 1 FAILED: Expected INVALID but got " + result1);
-        }
+            System.out.println("Test Case 1 FAILED: Expected INVALID but got " + result1);}
 
-        // --- เขียน Test Case อื่นๆ ต่อ ---
+        // Test Case 2: รหัสผ่านตัวอักษรตัวเล็กกับตัวเลข ตัวอัษรพิมพ์ใหญ่กับตัวเลข ตัวอักษรเล็กกับตัวอักษรใหญ่ควรจะ WEAK
+        PasswordStrength result2 = PasswordValidator.validate("fffff525");
+        if (result2 == PasswordStrength.WEAK) {
+            System.out.println("Test Case 2 Passed: Lowercase or Uppercase with number password is WEAK.");
+        } else {
+            System.out.println("Test Case 2 FAILED: Expected WEAK but got " + result2);}
+        
+        // Test Case 3: รหัสผ่านที่มีตัวเลข ตัวอักษรเล็กและอัษรพิมพ์ใหญ่ ควรจะ MEDIUM
+        PasswordStrength result3 = PasswordValidator.validate("Ddlfldk515");
+        if (result3 == PasswordStrength.MEDIUM) {
+            System.out.println("Test Case 3 Passed: Lowercase and Uppercase with number password is MEDIUM.");
+        } else {
+            System.out.println("Test Case 3 FAILED: Expected MEDIUM but got " + result3);}
+
+
+        // Test Case 4: รหัสผ่านมีทั้งมีตัวเลข ตัวอักษรเล็ก ตัวอักษรพิมพ์ใหญ่ และspecialchar ควรจะ STRONG
+        PasswordStrength result4 = PasswordValidator.validate("FFFddAA555");
+        if (result4 == PasswordStrength.STRONG) {
+            System.out.println("Test Case 4 Passed: Password is STRONG.");
+        } else {
+            System.out.println("Test Case 4 FAILED: Expected STRONG but got " + result4);}
+
 
         System.out.println("--------------------------------");
     }
